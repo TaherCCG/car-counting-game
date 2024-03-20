257308df4c07ef94cd24970ee1f5dd2d73584a7d
TaherCCG <taher.ccg@gmail.com>
2024-03-20 16:45:13 +0000

    Add sound effects in menu.js

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,5 +1,16 @@
+
+// play sound
+const soundSources = {
+    sound1: "assets/sounds/short-car-ignition.wav",
+    sound2: "assets/sounds/car-double-horn.wav"
+};
+function playSound(sound) {
+    const audio = new Audio(soundSources[sound]);
+    audio.play();
+}
 $('#card1').mouseenter(function () {
     $(this).find('.mode1').fadeIn(1000);
+    playSound("sound1");
 });
 
 $('#card1').mouseleave(function () {
@@ -8,16 +19,24 @@ $('#card1').mouseleave(function () {
 
 $('#card2').mouseenter(function () {
     $(this).find('.mode2').fadeIn(1000);
+    playSound("sound1");
 });
 
 $('#card2').mouseleave(function () {
     $(this).find('.mode2').fadeOut(2000);
 });
 
+// $('button').click(playSound("sound2"));
 
 document.getElementById("playMode1").addEventListener("click", function() {
-    window.location.href = "mode1.html";
+    playSound("sound2");
+    setTimeout(() => {
+        window.location.href = "mode1.html";
+    }, 1000);  
 });
 document.getElementById("playMode2").addEventListener("click", function() {
-    window.location.href = "mode2.html";
+    playSound("sound2");
+    setTimeout(() => {
+        window.location.href = "mode2.html";
+    }, 1000);
 });
