a1c118c355013bf7ee58871ca951a8b38b1c720e
TaherCCG <taher.ccg@gmail.com>
2024-03-21 11:47:58 +0000

    Refactor mouseenter and mouseleave event handlers in menu.js

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -8,22 +8,24 @@ function playSound(sound) {
     const audio = new Audio(soundSources[sound]);
     audio.play();
 }
+
 $('#card1').mouseenter(function () {
-    $(this).find('.mode1').fadeIn(1000);
+    $(this).find('.mode1').show(1000).fadeIn(1000);
     playSound("sound1");
 });
 
 $('#card1').mouseleave(function () {
-    $(this).find('.mode1').fadeOut(2000);
+    setTimeout
+    $(this).find('.mode1').hide(1000).fadeOut(1000);
 });
 
 $('#card2').mouseenter(function () {
-    $(this).find('.mode2').fadeIn(1000);
+    $(this).find('.mode2').show(1000).fadeIn(1000);
     playSound("sound1");
 });
 
 $('#card2').mouseleave(function () {
-    $(this).find('.mode2').fadeOut(2000);
+    $(this).find('.mode2').hide(1000).fadeOut(1000);
 });
 
 document.getElementById("playMode1").addEventListener("click", function() {
