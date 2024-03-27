f8df2df79bf22ffe8abe0f8708ac0a6dece12b4c
TaherCCG <taher.ccg@gmail.com>
2024-03-27 16:33:48 +0000

    Add: mode1.js file with initial functions

---
+++ assets/scripts/mode1.js
@@ -0,0 +1,20 @@
+const track = document.getElementById('track');
+const cars = {};
+const counters = {};
+
+let clickCounts = 0;
+let totalClicks = 0;
+
+
+// Function to move a car
+function moveCar(color) { };
+
+// Function to update click counts
+function updateClickCounts() { };
+
+// Function to play sound
+function playSound(sound) { };
+
+// Function to set up event listeners
+function setupEventListeners() { };
+
